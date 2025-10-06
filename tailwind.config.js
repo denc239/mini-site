7aa36d65aec5e07c09f381c60b8eef427dc40187
denc239 <46786447+denc239@users.noreply.github.com>
2025-10-07 00:24:37 +0300

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["app/**/*.jsx", "components/**/*.js", "pages/*/*/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00c6eb",
+        secondary: "#ffb000",
+        background: "#000",
+        text: "#fff",
+      },
+    }
+  },
+  plugins: []
+}
